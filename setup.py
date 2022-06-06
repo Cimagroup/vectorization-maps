b448382f637372e10a80f564c268d0b329c8447c
EduPH <epaluzo@us.es>
2022-06-06 17:43:38 +0200

    Added setup.py. To install pip install .

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="vectorisation", # Replace with your own username
+    version="1.0",
+    author="_",
+    author_email="epaluzo@us.es",
+    description="A TDA vectorisation Package",
+    url="https://github.com/Cimagroup/vectorisation-maps",
+    packages=setuptools.find_packages(),
+)
